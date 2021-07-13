861bfa5a980213bec9395ec815fa86eae38cba58
sneeu-leeu <sa070197@gmail.com>
2021-07-13 21:02:30 +0200

    style the main

--- src/index.js
+++ src/index.js
@@ -9,13 +9,17 @@ function renderTodo(todo) {
 
   const isChecked = todo.checked ? 'done' : '';
   const node = document.createElement('li');
-  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('class', `todo-item ${isChecked} border flex height pad-left`);
   node.setAttribute('data-key', todo.index);
   node.innerHTML = `
-    <input id="${todo.index}" type="checkbox"/>
-    <label for="${todo.index}" class="tick js-tick"></label>
-    <span>${todo.text}</span>
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-v" class="svg-inline--fa move move-js-todo fa-ellipsis-v fa-w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z"></path></svg>
+    <div class="left flex">
+      <input class="check" id="${todo.index}" type="checkbox"/>
+      <label for="${todo.index}" class="tick js-tick"></label>
+      <span>${todo.text}</span>
+    </div>
+    <div class="right flex">
+      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="ellipsis-v" class="svg-inline--fa move move-js-todo fa-ellipsis-v fa-w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z"></path></svg>
+    </div>
   `;
 
   list.append(node);
